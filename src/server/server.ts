28cb0f707ab9c2ac744f0eb789d305354d307206
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-08-15 20:47:02 +0200

    Fix routes

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.static('public'));
 app.use(apiRouter);
 
-// app.get('*', (req,res) =>{
-// 	res.sendFile(path.join(__dirname + '/../public/index.html'));
-// });
+app.get('*', (req,res) =>{
+	res.sendFile(path.join(__dirname + '/../public/index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
