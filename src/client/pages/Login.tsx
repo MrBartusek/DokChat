69b25451e1b5961b7ca47609553f18de5b97dd2e
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-08-18 17:53:02 +0200

    Start login page

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Col, Image, Row } from 'react-bootstrap';
 import Layout from '../components/Layout/Layout';
+import LoginForm from '../components/LoginForm/Login';
 
 export function Login() {
 	return (
 		<Layout>
-			<h2>Login</h2>
+			<Row className='my-5 py-5'>
+				<Col className='d-flex justify-content-center' style={{'height': 500}}>
+					<Image src="./img/undraw_login.svg" />
+				</Col>
+				<Col>
+					<LoginForm />
+				</Col>
+			</Row>
 		</Layout>
 
 	);
