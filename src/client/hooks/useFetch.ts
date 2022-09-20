c042b8af62875bedbf6dbc337edf998dcdd5c3ef
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-09-20 16:30:16 +0200

    Create useFetch.ts

---
+++ src/client/hooks/useFetch.ts
@@ -0,0 +1,18 @@
+//Stolen from: https://github.com/benawad/react-hooks-tutorial/blob/master/src/useForm.js
+
+import { useState } from 'react';
+
+export function useForm<T>(initialValues: T): [T, React.Dispatch<any>] {
+	const [values, setValues] = useState(initialValues);
+
+	return [
+		values,
+		e => {
+			const isCheckbox = e.target.type == 'checkbox';
+			setValues({
+				...values,
+				[e.target.name]: isCheckbox ? e.target.checked : e.target.value
+			});
+		}
+	];
+}
