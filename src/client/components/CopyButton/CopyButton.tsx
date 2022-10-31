be4010dbe9e3a3b9132b5ef4030ac2b883c6aa45
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-10-31 13:04:35 +0100

    Improve copy button tooltip

--- src/client/components/CopyButton/CopyButton.tsx
+++ src/client/components/CopyButton/CopyButton.tsx
@@ -3,6 +3,7 @@ import { OverlayTrigger, Tooltip, TooltipProps } from 'react-bootstrap';
 import { BsCheck, BsClipboard } from 'react-icons/bs';
 import useCopy from '../../hooks/useCopy';
 import IconButton, { IconButtonProps } from '../IconButton/IconButton';
+import UpdatingTooltip from '../UpdatingTooltip/UpdatingTooltip';
 
 interface CopyButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
 	copyText: string;
@@ -12,9 +13,9 @@ function CopyButton(props: CopyButtonProps) {
 	const [ copied, copyAction ] = useCopy(props.copyText);
 
 	const tooltip = (props: TooltipProps) => (
-		<Tooltip {...props}>
-			Copy to clipboard
-		</Tooltip>
+		<UpdatingTooltip {...props}>
+			{copied ? 'Copied!' : 'Copy to clipboard'}
+		</UpdatingTooltip>
 	);
 
 	const propsCopy = Object.assign({}, props);
---
+++ src/client/components/UpdatingTooltip/UpdatingTooltip.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { Tooltip } from 'react-bootstrap';
+
+const UpdatingTooltip = React.forwardRef(
+	({ popper, children, show: _, ...props }: any, ref) => {
+		useEffect(() => {
+			popper.scheduleUpdate();
+		}, [ children, popper ]);
+
+		return (
+			<Tooltip ref={ref} {...props}>
+				{children}
+			</Tooltip>
+		);
+	}
+);
+
+UpdatingTooltip.displayName = 'UpdatingTooltip';
+
+export default UpdatingTooltip;
