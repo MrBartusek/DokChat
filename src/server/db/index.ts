88a49d5b4faa9a0ce24628f68edc01fe70cfdc49
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-11-04 20:50:45 +0100

    Make db configurable

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,24 +2,19 @@ import { Pool, PoolConfig } from 'pg';
 
 let poolConfig: PoolConfig = {};
 
-if(process.env.NODE_ENV === 'production') {
-	poolConfig = {
-		connectionString: process.env.DATABASE_URL,
-		ssl: {
-			rejectUnauthorized: false
-		}
-	};
-}
-else {
-	poolConfig = {
-		user: 'postgres',
-		password: 'postgres',
-		host: 'localhost',
-		database: 'dokchat',
-		port: 5432
-	};
+export const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const host = process.env.DB_HOST;
+const database = process.env.DB_NAME;
+const port = process.env.DB_PORT as unknown as number;
 
-}
+poolConfig = {
+	user: user || 'postgres',
+	password: password || 'postgres',
+	host: host || 'localhost',
+	database: database || 'dokchat',
+	port: port || 5432
+};
 
 const pool = new Pool(poolConfig);
 
