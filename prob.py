d1964d952755c24a91a40b61613836616b453164
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-12-04 13:21:05 +0100

    Delete prob.py

--- prob.py
+++
@@ -1,8 +0,0 @@
-from math import log1p, sqrt
-
-def birthday(probability_exponent, bits):
-    probability = 10. ** probability_exponent
-    outputs     =  2. ** bits
-    return sqrt(2. * outputs * -log1p(-probability))
-
-print(birthday(-5,160))
