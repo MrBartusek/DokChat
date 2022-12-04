5e87127283039027271c83595fc45ccf1eea3543
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-12-04 23:04:20 +0100

    Fix commit short hash in debug info

--- src/client/components/DebugInfo/DebugInfo.tsx
+++ src/client/components/DebugInfo/DebugInfo.tsx
@@ -14,7 +14,7 @@ export default function DebugInfo() {
 		<div className='small text-muted mt-5 text-center'>
             DokChat ver. <code>
 				<a className='text-muted' href={`https://github.com/MrBartusek/DokChat/commit/${commitHash}`} target="_blank" rel="noreferrer">
-					{commitHash.substring(0, 8)}
+					{commitHash.substring(0, 7)}
 				</a>
 			</code> ({mode} build) <br />
             User: <code className='text-muted'>{user.id}</code> ({user.discriminator})
