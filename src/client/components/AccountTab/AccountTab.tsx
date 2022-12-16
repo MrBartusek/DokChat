3ce8d6c46e3ff854da59ae8329138398edd5ae33
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-12-16 22:54:53 +0100

    Gray out security settings

--- src/client/components/AccountTab/AccountTab.tsx
+++ src/client/components/AccountTab/AccountTab.tsx
@@ -12,20 +12,27 @@ export default function AccountTab() {
 				title="Password and Authentication"
 			>
 				<>
-					<InteractiveButton variant='primary' size='sm' icon={BsShieldLock}>
+					<div className='text-muted pb-3'>
+					Security options are not yet implemented. See {' '}
+						<a href="https://github.com/MrBartusek/DokChat/issues/10">#10</a> and {' '}
+						<a href="https://github.com/MrBartusek/DokChat/issues/29">#29</a>
+					</div>
+					<div className='opacity-25 pointer-event'>
+						<InteractiveButton variant='primary' size='sm' icon={BsShieldLock}>
 						Change Password
-					</InteractiveButton>
-					<p className='small fw-bold text-danger mt-4 mb-2'>
-						<BsUnlockFill className='me-1'/>
+						</InteractiveButton>
+						<p className='small fw-bold text-danger mt-4 mb-2'>
+							<BsUnlockFill className='me-1'/>
 						Two-factor authentication disabled
-					</p>
-					<p className='small text-muted mb-2'>
+						</p>
+						<p className='small text-muted mb-2'>
 						Two-factor authentication (2FA for short) is added security layer for your DokChat account.
 						When 2FA is enabled you need to confirm every login attempt.
-					</p>
-					<InteractiveButton variant='primary' size='sm' icon={BsShieldLock}>
+						</p>
+						<InteractiveButton variant='primary' size='sm' icon={BsShieldLock}>
 						Enable 2FA
-					</InteractiveButton>
+						</InteractiveButton>
+					</div>
 				</>
 			</SettingsRow>
 			<SettingsRow
