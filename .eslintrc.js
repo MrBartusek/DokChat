805f81fa8f03d8a7d4d5846d05e8e81ff3480d24
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-08-09 16:41:21 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
 	],
 	overrides: [
 	],
-	ignorePatterns: ['*.html'],
+	ignorePatterns: [ '*.html' ],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		'ecmaVersion': 'latest',
@@ -27,20 +27,20 @@ module.exports = {
 		}
 	},
 	rules: {
-		'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }],
-		'quotes': ['error', 'single'],
-		'no-trailing-spaces': ['error'],
-		'eol-last': ['error', 'always'],
-		'curly': ['error', 'multi-line', 'consistent'],
-		'indent': ['error', 'tab', { 'SwitchCase': 1, 'ignoredNodes': ['TemplateLiteral *'] }],
-		'semi': ['error', 'always'],
-		'no-multiple-empty-lines': ['error', { 'max': 1 }],
-		'comma-dangle': ['error', 'never'],
+		'brace-style': [ 'error', 'stroustrup', { 'allowSingleLine': true } ],
+		'quotes': [ 'error', 'single' ],
+		'no-trailing-spaces': [ 'error' ],
+		'eol-last': [ 'error', 'always' ],
+		'curly': [ 'error', 'multi-line', 'consistent' ],
+		'indent': [ 'error', 'tab', { 'SwitchCase': 1, 'ignoredNodes': [ 'TemplateLiteral *' ] } ],
+		'semi': [ 'error', 'always' ],
+		'no-multiple-empty-lines': [ 'error', { 'max': 1 } ],
+		'comma-dangle': [ 'error', 'never' ],
 		'@typescript-eslint/no-non-null-assertion': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-this-alias': 'off',
 		'@typescript-eslint/ban-ts-comment': 'off',
 		'@typescript-eslint/no-empty-interface': 'off',
-		'array-bracket-spacing': ['error', 'always']
+		'array-bracket-spacing': [ 'error', 'always' ]
 	}
 };
