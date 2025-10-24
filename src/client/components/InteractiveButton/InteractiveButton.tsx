eeec0b7c5654c8de4459639eb6deefa577e59904
Bartosz Dokurno <bartusekcraft@gmail.com>
2025-10-24 20:32:21 +0200

    Run lint

--- src/client/components/InteractiveButton/InteractiveButton.tsx
+++ src/client/components/InteractiveButton/InteractiveButton.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button, ButtonProps, Spinner } from "react-bootstrap";
-import { IconType } from "react-icons";
+import React from 'react';
+import { Button, ButtonProps, Spinner } from 'react-bootstrap';
+import { IconType } from 'react-icons';
 
 export interface InteractiveButtonProps extends ButtonProps {
   loading?: boolean;
@@ -9,26 +9,26 @@ export interface InteractiveButtonProps extends ButtonProps {
 }
 
 function InteractiveButton(props: InteractiveButtonProps) {
-  const { loading, icon, disabled, children, ...buttonProps } = props;
+	const { loading, icon, disabled, children, ...buttonProps } = props;
 
-  const spinner = (
-    <Spinner
-      as="span"
-      animation="border"
-      size="sm"
-      role="status"
-      aria-hidden="true"
-      className="me-2"
-    />
-  );
-  const iconEl = icon ? React.createElement(icon, { className: "me-2" }) : null;
+	const spinner = (
+		<Spinner
+			as="span"
+			animation="border"
+			size="sm"
+			role="status"
+			aria-hidden="true"
+			className="me-2"
+		/>
+	);
+	const iconEl = icon ? React.createElement(icon, { className: 'me-2' }) : null;
 
-  return (
-    <Button disabled={disabled || loading} {...(buttonProps as any)}>
-      {loading ? spinner : iconEl}
-      {children}
-    </Button>
-  );
+	return (
+		<Button disabled={disabled || loading} {...(buttonProps as any)}>
+			{loading ? spinner : iconEl}
+			{children}
+		</Button>
+	);
 }
 
 export default InteractiveButton;
